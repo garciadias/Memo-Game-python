f97bf7b2e8d1ebfac73ab20742171d630cd75ba6
R. Garcia-Dias <rafaelagd@gmail.com>
2019-01-26 13:54:30 +0000

    gree: Should be able flip a card

--- memo_game.py
+++ memo_game.py
@@ -1,8 +1,23 @@
 
-class MemoGame:
+class Card():
+    
+    def __init__(self, facing_up):
+        self.facing_up = facing_up
+    
+
+class MemoGame():
 
     def __init__(self):
         pass
 
-    def facing_up(self):
+    def face_up_card(self, card):
+        pass
+
+    def get_card(self, card):
+        return Card(facing_up=True)
+
+    def count_facing_up(self):
         return 0
+
+    def get_all_cards(self):
+        return [Card(True)]
--- test_memo-game.py
+++ test_memo-game.py
@@ -1,9 +1,20 @@
-"""Game setup
-
-    All cards are facing down
-"""
-
 from memo_game import MemoGame
 
+
+def getRandomCardFrom(memoGame):
+    all_cards = memoGame.get_all_cards()
+    return all_cards[0]
+
+
 def test_all_cards_facing_down():
-    assert MemoGame().facing_up() == 0
+    assert MemoGame().count_facing_up() == 0
+
+
+def test_should_be_able_to_flip_one_card():
+    aMemoGame = MemoGame()
+    randomCard = getRandomCardFrom(aMemoGame)
+
+    aMemoGame.face_up_card(randomCard)
+    theCard = aMemoGame.get_card(randomCard)
+
+    assert theCard.facing_up is True
